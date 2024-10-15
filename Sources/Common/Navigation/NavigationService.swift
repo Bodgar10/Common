d2e44f07e3d8d9020492e42f48b19d3813dc96a3
Bodgar Espinosa <bodgar_jair@hotmail.com>
2024-10-14 20:57:17 -0600

    Added navigationState

--- Sources/Common/Navigation/NavigationService.swift
+++ Sources/Common/Navigation/NavigationService.swift
@@ -5,13 +5,23 @@
 //  Created by Bodgar Espinosa Miranda on 07/08/24.
 //
 
+import Foundation
+
+public class NavigationState: ObservableObject {
+    @Published var path: [Destination] = []
+    @Published var isPresentingModal: Bool = false
+    @Published var modalDestination: Destination?
+}
 
 /// Protocol to know where we need to go in each screen.
 public protocol Destination {}
 
 /// Service to use in the different modules to create each router in each module.
-public protocol NavigationService: Service {
+public protocol NavigationService: Service, ObservableObject {
+    var navigationState: NavigationState { get }
     func navigate(to destination: Destination)
+    func presentModal(to destination: Destination)
+    func dismissModal()
 }
 
 protocol Test {
